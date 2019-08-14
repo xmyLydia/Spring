9566e779886a336c51814f585740147062faa618
Mingyue Xie <836728902@qq.com>
2019-08-14 23:53:02 +0300

    Hibernate CRUD project
    CRUD about customer in the database

---
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,63 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Customer> getCustomers() {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// create a query, sort by the last name
+		Query<Customer> query = currentSession
+				.createQuery("from Customer order by lastName", Customer.class);
+		// execute query and get result list
+		List<Customer> customers = query.getResultList();
+		// return the results
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		// get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// save the customer to db
+		currentSession.saveOrUpdate(customer);
+	}
+
+	@Override
+	public Customer getCustomer(int id) {
+		// get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// retrieve/read from db using the primary key
+		Customer customer = currentSession.get(Customer.class, id);
+		return customer;
+	}
+
+	@Override
+	public void deleteCustomer(int id) {
+		// get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+	   //delete the customer
+		Query query = currentSession.createQuery("delete from Customer where id=:customerId");
+		query.setParameter("customerId", id);
+		
+		query.executeUpdate();
+	}
+
+}
